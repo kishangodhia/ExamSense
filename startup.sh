acd97b8f269db5bf1031f7115eedd16eba29061e
Kishan Godhia <godhiaroshan@gmail.com>
2026-02-22 20:03:29 +0530

    Fix Azure OCR paths

--- startup.sh
+++ startup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
 apt-get update
 apt-get install -y tesseract-ocr poppler-utils
+
+export TESSDATA_PREFIX=/usr/share/tesseract-ocr/4.00/tessdata
+export PATH="/usr/bin:$PATH"
+
 streamlit run app.py --server.port $PORT --server.address 0.0.0.0
